ff44ffb255d63773895e5b9244dc22712ef794d7
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-08-20 15:06:29 +0700

    Show error for missing credentials on LoginFragment

--- app/src/main/java/com/zartre/app/healthy/LoginFragment.java
+++ app/src/main/java/com/zartre/app/healthy/LoginFragment.java
@@ -4,14 +4,44 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button _loginBtn = getView().findViewById(R.id.login_btn_login);
+        _loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText _userId = getView().findViewById(R.id.login_input_username);
+                EditText _password = getView().findViewById(R.id.login_input_password);
+                String _userIdStr = _userId.getText().toString();
+                String _passwordStr = _password.getText().toString();
+
+                if (_userIdStr.isEmpty() || _passwordStr.isEmpty()) {
+                    Toast.makeText(getActivity(), "Missing username or password", Toast.LENGTH_SHORT)
+                            .show();
+                    Log.d("USER", "Missing username or password");
+                } else if (_userIdStr.equalsIgnoreCase("admin") && _passwordStr.equalsIgnoreCase("admin")) {
+                    Log.d("USER", "Go to BMI");
+                }
+            }
+
+        });
     }
 }
