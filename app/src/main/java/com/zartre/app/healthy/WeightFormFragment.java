c66c93bf30a1e14a82f41cb6d24016d9f5c684b9
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-10 08:58:53 +0700

    Update WeightFormFragment.java and fragment_weight_form.xml

--- app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
+++ app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
@@ -1,4 +1,22 @@
 package com.zartre.app.healthy;
 
-public class WeightFormFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeightFormFragment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weight_form, container, false);
+    }
 }
