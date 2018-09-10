ae0f428b01f5de622f0f405255621666eede80e6
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-10 13:41:01 +0700

    Add Firebase SDK

--- app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
+++ app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
@@ -7,11 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class WeightFormFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Button _addBtn = getView().findViewById(R.id.weight_btn_add);
+        _addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Nullable
