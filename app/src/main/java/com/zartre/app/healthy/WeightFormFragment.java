3648936b3ee1964d1cf752ba7efc45f45bbe3083
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-17 14:26:14 +0700

    Log weight to Firestore

--- app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
+++ app/src/main/java/com/zartre/app/healthy/WeightFormFragment.java
@@ -4,21 +4,64 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class WeightFormFragment extends Fragment {
+
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
         Button _addBtn = getView().findViewById(R.id.weight_form_btn_add);
         _addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
+                EditText _date = getView().findViewById(R.id.weight_form_input_date);
+                EditText _weight = getView().findViewById(R.id.weight_form_input_weight);
+                String _dateStr = _date.getText().toString();
+                String _weightStr = _date.getText().toString();
+
+                WeightRecord _record = new WeightRecord();
+                _record.setDate(_dateStr);
+                _record.setWeight(_weightStr);
+
+                firestore.collection("myfitness")
+                        .document(auth.getCurrentUser().getUid())
+                        .collection("weight")
+                        .document(_dateStr)
+                        .set(_record)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                getFragmentManager()
+                                        .beginTransaction()
+                                        // TODO: Change WeightFormFragment to WeightFragment
+                                        .replace(R.id.main_view, new WeightFormFragment())
+                                        .commit();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Couldn't log your weight", Toast.LENGTH_SHORT).show();
+                                Log.d("WEIGHT", e.getMessage());
+                            }
+                        });
             }
         });
     }
---
+++ app/src/main/java/com/zartre/app/healthy/WeightRecord.java
@@ -0,0 +1,23 @@
+package com.zartre.app.healthy;
+
+public class WeightRecord {
+
+    String weight;
+    String date;
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
