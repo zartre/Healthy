5aac762f3fc9ca6d8884597e4198e7177d1e63dc
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-23 15:50:38 +0700

    Add BMI calculation action

--- app/src/main/java/com/zartre/app/healthy/BmiFragment.java
+++ app/src/main/java/com/zartre/app/healthy/BmiFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BmiFragment extends Fragment {
     @Nullable
@@ -22,6 +25,28 @@ public class BmiFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         Log.d("SCREEN", "BmiFragment onActivityCreated");
         super.onActivityCreated(savedInstanceState);
-        Button _calcBtn = getView().findViewById(R.id.bmi_btn_calculate);
+        final EditText _height = getView().findViewById(R.id.bmi_input_height);
+        final EditText _weight = getView().findViewById(R.id.bmi_input_weight);
+        final TextView _bmi = getView().findViewById(R.id.bmi_txt_calculated_bmi);
+        final Button _calcBtn = getView().findViewById(R.id.bmi_btn_calculate);
+
+        _calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (_height.getText().toString().isEmpty() || _weight.getText().toString().isEmpty()) {
+                    Log.d("BMI", "Fields height and weight are empty");
+                    Toast.makeText(getActivity(), "Please fill in all the fields", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    Double heightInt = Double.parseDouble(_height.getText().toString()) / 100;
+                    Double weightInt = Double.parseDouble(_weight.getText().toString());
+                    Double bmi = weightInt / (heightInt * heightInt);
+
+                    Log.d("BMI", "BMI is " + bmi);
+                    _bmi.setVisibility(View.VISIBLE);
+                    _bmi.setText(String.format("%.2f", bmi));
+                }
+            }
+        });
     }
 }
