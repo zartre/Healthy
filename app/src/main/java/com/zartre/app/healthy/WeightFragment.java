9ab697e8301f0f9bc468d396054c0c4e59ac8dab
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-23 13:19:36 +0700

    Get weight records from Firestore

--- app/src/main/java/com/zartre/app/healthy/WeightFragment.java
+++ app/src/main/java/com/zartre/app/healthy/WeightFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.*;
 
 import java.util.ArrayList;
 
 public class WeightFragment extends Fragment {
     ArrayList<WeightRecord> weightRecords = new ArrayList<>();
+    FirebaseFirestore firestore;
+    FirebaseAuth firebaseAuth;
 
     @Nullable
     @Override
@@ -25,19 +31,31 @@ public class WeightFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        WeightRecord w1 = new WeightRecord();
-        w1.setWeight("46");
-        w1.setDate("21-09-2018");
-        WeightRecord w2 = new WeightRecord();
-        w2.setWeight("45");
-        w2.setDate("22-09-2018");
-        weightRecords.add(w1);
-        weightRecords.add(w2);
+        final ListView _weightList = getView().findViewById(R.id.weight_list);
 
-        ListView _weightList = getView().findViewById(R.id.weight_list);
+        firestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        WeightAdapter weightAdapter = new WeightAdapter(getActivity(), R.layout.fragment_weight_card, weightRecords);
-        _weightList.setAdapter(weightAdapter);
+        firestore.collection("myfitness")
+                .document(firebaseAuth.getCurrentUser().getUid())
+                .collection("weight")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Toast.makeText(getActivity(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                                WeightRecord tempWeight = new WeightRecord();
+                                tempWeight.setDate(doc.get("date").toString());
+                                tempWeight.setWeight(doc.get("weight").toString());
+                                weightRecords.add(tempWeight);
+                            }
+                            WeightAdapter weightAdapter = new WeightAdapter(getActivity(), R.layout.fragment_weight_card, weightRecords);
+                            _weightList.setAdapter(weightAdapter);
+                        }
+                    }
+                });
 
         Button _addWeightBtn = getView().findViewById(R.id.weight_btn_add);
         _addWeightBtn.setOnClickListener(new View.OnClickListener() {
