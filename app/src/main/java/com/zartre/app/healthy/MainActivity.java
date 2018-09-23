604d3017ea6d41dbd7628271a17ac47bf3d77cd2
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-23 10:30:25 +0700

    Open with Menu fragment when user is already logged in

--- app/src/main/java/com/zartre/app/healthy/MainActivity.java
+++ app/src/main/java/com/zartre/app/healthy/MainActivity.java
@@ -3,19 +3,32 @@ package com.zartre.app.healthy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         Log.d("LOGIN", "Application launched");
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_view, new LoginFragment())
-                    .commit();
+            if (firebaseAuth.getCurrentUser() != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new MenuFragment())
+                        .commit();
+            } else {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new LoginFragment())
+                        .commit();
+            }
         }
     }
 }
