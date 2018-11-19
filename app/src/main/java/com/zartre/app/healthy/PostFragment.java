30c7fa284c3a67b33ad3611e1a07f25d8b4b2e67
DobaKung <zartre.y@gmail.com>
2018-11-19 16:10:08 +0700

    Add  layouts for Post

---
+++ app/src/main/java/com/zartre/app/healthy/PostFragment.java
@@ -0,0 +1,8 @@
+package com.zartre.app.healthy;
+
+import android.support.v4.app.Fragment;
+import okhttp3.OkHttpClient;
+
+public class PostFragment extends Fragment {
+    private OkHttpClient okHttpClient = new OkHttpClient();
+}
