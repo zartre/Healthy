e122105372866ec5b8b194055c4de57a3ff27a10
DobaKung <zartre.y@gmail.com>
2018-11-19 16:39:54 +0700

    Add getView method to PostAdapter

--- app/src/main/java/com/zartre/app/healthy/adapter/PostAdapter.java
+++ app/src/main/java/com/zartre/app/healthy/adapter/PostAdapter.java
@@ -2,7 +2,13 @@ package com.zartre.app.healthy.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.zartre.app.healthy.R;
 import com.zartre.app.healthy.data.Post;
 
 import java.util.List;
@@ -15,5 +21,18 @@ public class PostAdapter extends ArrayAdapter<Post> {
         super(context, resource, objects);
         this.context = context;
         this.posts = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final View _postItem = LayoutInflater.from(context).inflate(R.layout.fragment_post_card, parent, false);
+        final TextView _title = _postItem.findViewById(R.id.post_card_title);
+        final TextView _body = _postItem.findViewById(R.id.post_card_content);
+        final Post post = posts.get(position);
+        final String titleFormat = context.getString(R.string.post_card_title);
+        _title.setText(String.format(titleFormat, post.getId(), post.getTitle()));
+        _body.setText(post.getBody());
+        return _postItem;
     }
 }
