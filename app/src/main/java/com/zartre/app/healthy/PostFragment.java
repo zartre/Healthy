ae6938ff617ecefb1d827150a2b2b9d72471ec6e
DobaKung <zartre.y@gmail.com>
2018-11-19 19:00:08 +0700

    Convert JSON to Post object

--- app/src/main/java/com/zartre/app/healthy/PostFragment.java
+++ app/src/main/java/com/zartre/app/healthy/PostFragment.java
@@ -5,24 +5,32 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toolbar;
+import com.zartre.app.healthy.data.Post;
 import com.zartre.app.healthy.task.GetRestIntentService;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostFragment extends Fragment {
     private static final String TAG = "PostFragment";
     private final String POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
     private OkHttpClient okHttpClient = new OkHttpClient();
 
+    private static List<Post> posts = new ArrayList<>();
+
     private Toolbar _toolbar;
+    private RecyclerView _postList;
 
     @Nullable
     @Override
@@ -35,10 +43,13 @@ public class PostFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         _toolbar = getView().findViewById(R.id.posts_toolbar);
+        _postList = getView().findViewById(R.id.posts_list);
 
         createToolbar();
 
+        // fetch posts
         final Intent fetchIntent = new Intent(getActivity(), GetRestIntentService.class);
+        fetchIntent.putExtra(GetRestIntentService.PARAM_IN_URL, POSTS_URL);
         getActivity().startService(fetchIntent);
     }
 
@@ -53,14 +64,23 @@ public class PostFragment extends Fragment {
         });
     }
 
-    private String fetchPosts(String url) {
+    public static void onReceiveResult(String JsonResult) {
         try {
-            Request request = new Request.Builder().url(url).build();
-            Response response = okHttpClient.newCall(request).execute();
-            return response.body().string();
-        } catch (IOException e) {
-            Log.d(TAG, "fetchPosts: " + e.getLocalizedMessage());
-            return e.getLocalizedMessage();
+            final JSONArray jsonArray = new JSONArray(JsonResult);
+            final int ARR_LENGTH = jsonArray.length();
+            for (int i = 0; i < ARR_LENGTH; i++) {
+                final JSONObject postJsonObject = jsonArray.getJSONObject(i);
+                Post post = new Post(
+                        postJsonObject.getInt("id"),
+                        postJsonObject.getString("title"),
+                        postJsonObject.getString("body")
+                );
+                posts.add(post);
+            }
+        } catch (JSONException e) {
+            Log.d(TAG, "onReceiveResult: " + e.getLocalizedMessage());
         }
     }
+
+    private void updateView() {}
 }
--- app/src/main/java/com/zartre/app/healthy/task/GetRestIntentService.java
+++ app/src/main/java/com/zartre/app/healthy/task/GetRestIntentService.java
@@ -2,12 +2,21 @@ package com.zartre.app.healthy.task;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import com.zartre.app.healthy.PostFragment;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class GetRestIntentService extends IntentService {
-    private static final String TAG = "GetRestIntentService";
+    public static final String TAG = "GetRestIntentService";
+    public static final String PARAM_IN_URL = "url";
+
+    private OkHttpClient okHttpClient = new OkHttpClient();
+
     public GetRestIntentService() {
         super("GetRestIntentService");
     }
@@ -15,7 +24,16 @@ public class GetRestIntentService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Log.d(TAG, "onHandleIntent");
-        SystemClock.sleep(5000);
-        Log.d(TAG, "onHandleIntent: done");
+        final String url = intent.getStringExtra(PARAM_IN_URL);
+        Log.d(TAG, "onHandleIntent: " + url);
+        try {
+            Request request = new Request.Builder().url(url).build();
+            Response response = okHttpClient.newCall(request).execute();
+            String result = response.body() != null ? response.body().string() : "";
+            Log.d(TAG, "onHandleIntent: succeed");
+            PostFragment.onReceiveResult(result);
+        } catch (IOException e) {
+            Log.d(TAG, "onHandleIntent: " + e.getLocalizedMessage());
+        }
     }
 }
